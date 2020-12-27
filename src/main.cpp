df0150b800ba315187d6627717bde2943da9a399
hollowj <hollowj@syneforge.com>
2020-12-27 18:10:31 +0200

    done

--- src/main.cpp
+++ src/main.cpp
@@ -1,104 +1,66 @@
 #include <Arduino.h>
 
-const int redPort = 12;
-const int yellowPort = 11;
-const int greenPort = 10;
+using namespace std;
 
-const int garlandLength = 6;
-const int colorsNumber = 4;
+const int katode1 = 11;
+const int katode2 = 12;
 
-const int ports[garlandLength] {11, 10, 9, 6, 5, 3};
+
+const int pinLedStart = 2;
+const int pinLedSEnd = 8;
+
+const int pinPot = A0;
+
+byte numberToLedMap[10] = {
+        0b00111111,
+        0b00000110,
+        0b01011011,
+        0b01001111,
+        0b01100110,
+        0b01101101,
+        0b01111101,
+        0b00000111,
+        0b01111111,
+        0b01101111,
+};
 
 void setup() {
-    for (int port : ports) {
-        pinMode(port, OUTPUT);
-    }
-}
+    Serial.begin(9600);
 
-void applySimpleToPin(int pin) {
-    digitalWrite(pin, HIGH);
-    delay(1000);
-    digitalWrite(pin, LOW);
-    delay(1000);
-}
-
-void simpleMode() {
-    for (int port : ports) {
-        applySimpleToPin(port);
-    }
-}
-
-void applyFadeToPin(int pin) {
-    for (int i = 0; i < 255; i++) {
-        analogWrite(pin, i);
-        delay(5);
-    }
-    digitalWrite(pin, HIGH);
-
-    for (int i = 255; i > 0; i--) {
-        analogWrite(pin, i);
-        delay(5);
+    for (int i = pinLedStart; i <= pinLedSEnd; i++) {
+        pinMode(i, OUTPUT);
     }
 
-    digitalWrite(pin, LOW);
-}
-
-void fadeMode() {
-    for (int port : ports) {
-        applyFadeToPin(port);
-    }
-}
-
-void applyFastToPin(int pin) {
-    for (int i = 0; i < 255; i++) {
-        analogWrite(pin, i);
-        delay(1);
-    }
-    digitalWrite(pin, HIGH);
-
-    for (int i = 255; i > 0; i--) {
-        analogWrite(pin, i);
-        delay(1);
-    }
-
-    digitalWrite(pin, LOW);
-}
-
-void fastMode() {
-    for (int port : ports) {
-        applyFastToPin(port);
-    }
-
-    for (int port : ports) {
-        applyFastToPin(port);
-    }
-
-    for (int port : ports) {
-        applyFastToPin(port);
-    }
-}
-
-void allFadeMode() {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 255; j++) {
-            for (int port : ports) {
-                analogWrite(port, j);
-            }
-            delay(5);
-        }
-
-        for (int j = 255; j > 0; j--) {
-            for (int port : ports) {
-                analogWrite(port, j);
-            }
-            delay(5);
-        }
-    }
+    pinMode(katode1, OUTPUT);
+    pinMode(katode2, OUTPUT);
 }
 
 void loop() {
-    simpleMode();
-    fadeMode();
-    allFadeMode();
-    fastMode();
+    int number = analogRead(pinPot);
+
+    Serial.println(number);
+
+    number = map(number, 0, 1020, 0, 9);
+
+    Serial.println(number);
+
+    int mask = numberToLedMap[number];
+
+    digitalWrite(katode1, HIGH);
+    digitalWrite(katode2, LOW);
+
+    for (int i = 0; i <= (pinLedSEnd - pinLedStart); i++) {
+        digitalWrite(pinLedStart + i, bitRead(mask, i));
+    }
+
+    delay(5);
+
+    digitalWrite(katode1, LOW);
+    digitalWrite(katode2, HIGH);
+
+    for (int i = 0; i <= (pinLedSEnd - pinLedStart); i++) {
+        digitalWrite(pinLedStart + i, bitRead(mask, i));
+    }
+
+    delay(5);
 }
