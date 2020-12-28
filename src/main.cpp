b3bb8bdbf140f8ca7823b31285847f44e671de16
hollowj <hollowj@syneforge.com>
2020-12-28 23:23:08 +0200

    done

--- src/main.cpp
+++ src/main.cpp
@@ -1,104 +1,47 @@
 #include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
 
-const int redPort = 12;
-const int yellowPort = 11;
-const int greenPort = 10;
+const byte displayAddress = 0x27;
 
-const int garlandLength = 6;
-const int colorsNumber = 4;
+byte load0[8] = {0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000};
+byte load1[8] = {0b10000, 0b10000, 0b10000, 0b10000, 0b10000, 0b10000, 0b10000, 0b10000};
+byte load2[8] = {0b11000, 0b11000, 0b11000, 0b11000, 0b11000, 0b11000, 0b11000, 0b11000};
+byte load3[8] = {0b11100, 0b11100, 0b11100, 0b11100, 0b11100, 0b11100, 0b11100, 0b11100};
+byte load4[8] = {0b11110, 0b11110, 0b11110, 0b11110, 0b11110, 0b11110, 0b11110, 0b11110};
+byte load5[8] = {0b11111, 0b11111, 0b11111, 0b11111, 0b11111, 0b11111, 0b11111, 0b11111};
 
-const int ports[garlandLength] {11, 10, 9, 6, 5, 3};
+int percent = 0;
+
+LiquidCrystal_I2C lcd(displayAddress, 16, 2);
 
 void setup() {
-    for (int port : ports) {
-        pinMode(port, OUTPUT);
-    }
-}
-
-void applySimpleToPin(int pin) {
-    digitalWrite(pin, HIGH);
-    delay(1000);
-    digitalWrite(pin, LOW);
-    delay(1000);
-}
-
-void simpleMode() {
-    for (int port : ports) {
-        applySimpleToPin(port);
-    }
-}
-
-void applyFadeToPin(int pin) {
-    for (int i = 0; i < 255; i++) {
-        analogWrite(pin, i);
-        delay(5);
-    }
-    digitalWrite(pin, HIGH);
-
-    for (int i = 255; i > 0; i--) {
-        analogWrite(pin, i);
-        delay(5);
-    }
-
-    digitalWrite(pin, LOW);
-}
-
-void fadeMode() {
-    for (int port : ports) {
-        applyFadeToPin(port);
-    }
-}
-
-void applyFastToPin(int pin) {
-    for (int i = 0; i < 255; i++) {
-        analogWrite(pin, i);
-        delay(1);
-    }
-    digitalWrite(pin, HIGH);
-
-    for (int i = 255; i > 0; i--) {
-        analogWrite(pin, i);
-        delay(1);
-    }
-
-    digitalWrite(pin, LOW);
-}
-
-void fastMode() {
-    for (int port : ports) {
-        applyFastToPin(port);
-    }
-
-    for (int port : ports) {
-        applyFastToPin(port);
-    }
-
-    for (int port : ports) {
-        applyFastToPin(port);
-    }
-}
-
-void allFadeMode() {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 255; j++) {
-            for (int port : ports) {
-                analogWrite(port, j);
-            }
-            delay(5);
-        }
-
-        for (int j = 255; j > 0; j--) {
-            for (int port : ports) {
-                analogWrite(port, j);
-            }
-            delay(5);
-        }
-    }
+    lcd.init();
+    lcd.backlight();
+    lcd.createChar(0, load0);
+    lcd.createChar(1, load1);
+    lcd.createChar(2, load2);
+    lcd.createChar(3, load3);
+    lcd.createChar(4, load4);
+    lcd.createChar(5, load5);
 }
 
 void loop() {
-    simpleMode();
-    fadeMode();
-    allFadeMode();
-    fastMode();
+    lcd.clear();
+
+    for (int i = 0; i <= 16; i++) {
+        for (int k = 0; k <= 5; k++) {
+            lcd.setCursor(4, 0);
+            lcd.print("Loading ");
+            lcd.print(percent++);
+            lcd.setCursor(i, 1);
+            lcd.write(k);
+            delay(250);
+
+            if (percent > 100) {
+                delay(1000);
+                percent = 0;
+                return;
+            }
+        }
+    }
 }
